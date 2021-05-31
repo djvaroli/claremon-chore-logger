77630a901f98719378031b56a3e50d53b22ac5c6
Daniel Varoli <daniel.varoli@gmail.com>
2021-05-31 10:56:37 -0400

    more edits to nginx configuration

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 [[ -z "$PORT" ]] && export PORT=8080
-envsubst "$PORT" < /.ngnix/nginx.conf.in > /.nginx/nginx.conf
+envsubst '$PORT' < .nginx/nginx.conf.in > etc/nginx/nginx.conf
 
-exec nginx -c /.ngnix/nginx.conf
+exec nginx -c nginx.conf
